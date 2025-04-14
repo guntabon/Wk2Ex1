11654875f29b332c076b1e115f861c6775ddc281
Gunner Flory <g0364875@amarillocollege.com>
2025-04-14 13:04:47 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Interest Calculator Program");
+
+            Console.WriteLine("What is the principal amount of the loan in dollars?");
+
+            int myPrin = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the interest rate?");
+
+            double myRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is the period of loan in years?");
+
+            int period = Convert.ToInt32(Console.ReadLine());
+
+            double totalInterest = Convert.ToDouble(myPrin * myRate * period);
+
+            Console.WriteLine($"Total interest of Loan: {totalInterest}");
+
+
+        }
+    }
+}
